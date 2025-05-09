589a6dfaab946167fd4cc04d0d7389705d04c373
MarcoVacchi <mvacchi816@gmail.com>
2025-05-09 14:34:16 +0200

    feat: added env.example and connected database

---
+++ data/movies_db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+
+    if (err) throw err;
+    console.log('Mysql connesso con successo!');
+
+});
+
+module.exports = connection;
